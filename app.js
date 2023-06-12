998e51f164386fee2e0eca31ba16d16111153c62
dnava24 <61266103+dnava24@users.noreply.github.com>
2023-06-12 00:00:33 -0500

    Updated playRound function created endGame function
    updated to playRound and endGame functions

--- app.js
+++ app.js
@@ -11,45 +11,67 @@ function getComputerChoice(){
 }
 
 // play round function
-
 function playRound(playerSelection){
-    computerSelection = getComputerChoice();
-    let result;
+    const computerSelection = getComputerChoice();
+    let roundResult;
 
     if(playerSelection === computerSelection){
-        return `Try again, its a TIEEEE!`
+        roundResult = (`Try again, its a TIEEEE!`);
 }
     else if(
     (playerSelection === `rock` && computerSelection === `scissors`) ||
     (playerSelection === `paper` && computerSelection === `rock`) ||
     (playerSelection === `scissors` && computerSelection === `paper`)){
-        return `${playerSelection} wins!`
+        roundResult = (`${playerSelection} wins!`);
+        playerScore++;
+        round++;
     }
     else {
-         `${computerSelection} wins!`
+        roundResult = (`${computerSelection} wins!`);
         computerScore++;
         round++;}
+
+//display round results
+const roundResultElement = document.createElement('p');
+roundResultElement.textContent = " You chose " + playerSelection + " and the computer chose " + computerSelection;
+document.getElementById('round-result').textContent = "";
+document.getElementById('round-result').appendChild(roundResultElement);
+
+
+//update score
+document.getElementById('player-score').textContent = playerScore;
+document.getElementById('computer-score').textContent = computerScore;
+//check if total round have been played
+if(round === totalRound){
+    endGame();
+}
+return roundResult;
+
 }
 
-//function to handle button click for rock, paper, or scissors and pass the result to the playRound function
+//end game function 
 
-
-
-
-
-
-//display results
-
-
-
-
-
+function endGame(){
+    //hide result
+    document.getElementById('result').style.display = 'none';
+    //show button #play-again
+    document.getElementById('play-again').style.display = 'inline-block';
+   //show end game message
+    const endGameElement = document.createElement('h2');
+    endGameElement.textContent = "Congratulations! You won the game!";
+    document.getElementById('result').textContent='';
+    document.getElementById('result').appendChild(endGameElement);
+}
 // keep round count max of 5
 
-
-
-
-
-
-
-// hide play agin button
+function restartGame(){
+    playerScore = 0;
+    computerScore = 0;
+    round = 0;
+    document.getElementById('player-score').textContent = 0;
+    document.getElementById('computer-score').textContent = 0;
+    document.getElementById('result').innerHTML = '';
+    document.getElementById('play-again').style.display = 'none';
+//hide button #play-again
+document.getElementById('play-again').style.display = 'none';
+}
